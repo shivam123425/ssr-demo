cac1cc0d89eaa1f8d2aef138ef8e0641af858f1b
shivam123425 <shivam123425@gmail.com>
2019-07-30 23:42:59 +0530

    Add links to auth page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 import React from "react";
+import Link from "next/link";
+import Router from "next/router";
 
 const indexPage = () => (
   <div>
     <h1>The Main Page</h1>
+    <p>
+      Go To{" "}
+      <Link href="/auth">
+        <a>Auth</a>
+      </Link>
+      <button onClick={() => Router.push("/auth")}>Go to Auth</button>
+    </p>
   </div>
 );
 
