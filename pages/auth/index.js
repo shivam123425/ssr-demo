cff68d46b95ff6558564aafc894494fcf9d0147c
shivam123425 <shivam123425@gmail.com>
2019-07-30 23:49:17 +0530

    Create and include a component

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,10 @@
 import React from "react";
+import User from "../../components/User";
 
 const authIndexPage = () => (
   <div>
     <h1>The Auth Index Page</h1>
+    <User name="Shivam Kumar" age={28} />
   </div>
 );
 
